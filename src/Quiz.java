6d5b0eeb60f5ec753742121f5e1f1bf6dd90d147
timohaverich <timo.haverich@fh-bielefeld.de>
2022-10-27 11:59:50 +0200

    Aktuell

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,8 +1,10 @@
 public class Quiz {
     public static void main(String[] args) {
+        // Initialisierung eines neuen Rechenquiz
         RechenQuiz quiz = new RechenQuiz();
         quiz.getExercise();
         int result = quiz.getResult();
+        // gibt Ergebnis der Zufallsaufgabe aus
         System.out.println("Ergebnis: "+result);
     }
 }
--- src/RechenQuiz.java
+++ src/RechenQuiz.java
@@ -1,45 +1,56 @@
 public class RechenQuiz {
 
+    // Variablen zur Speicherung der Operanden
     int operand1;
     int operand2;
 
+    // Variablen zur Speicherung des Wertebereichs der Operanden
     int operandMin = 1;
     int operandMax = 20;
-    int operatorNum;
-    private String operator;
 
+    // Variable zur Speicherung des Operator-Indizes
+    int operatorNum;
+
+    // Variable zur Speicherung des Operators als Zeichen
+    String operator;
+
+    // Variable zur Speicherung des Ergebnisses
     int result;
 
-    public void createExercise() {
-        operand1 = operandMin + (int)(Math.random() * ((operandMax - operandMin) + operandMin));
-        operand2 = operandMin + (int)(Math.random() * ((operandMax - operandMin) + operandMin));
-        operatorNum =  1 + (int)(Math.random() * ((3 - 1) + 1));
-
-        switch (operatorNum) {
-            case 1:
-                operator = "+";
-                result = operand1+operand2;
-                break;
-            case 2:
-                operator = "-";
-                result = operand1-operand2;
-                break;
-            case 3:
-                operator = "*";
-                result = operand1*operand2;
-                break;
-        }
-    }
+    // Getter-Methode zur Generierung einer zufälligen Rechenaufgabe
     public void getExercise() {
-
         do {
-            createExercise();
+            // generiert zufällige Zahl für Operanden zwischen [1,20]
+            operand1 = operandMin + (int)(Math.random() * ((operandMax - operandMin) + operandMin));
+            operand2 = operandMin + (int)(Math.random() * ((operandMax - operandMin) + operandMin));
+
+            // generiert zufällige Zahl für die Zuweisung eines Operators
+            operatorNum =  1 + (int)(Math.random() * ((3 - 1) + 1));
+
+            // berechnet die Zufallsaufgabe und weist Operator zu
+            switch (operatorNum) {
+                case 1:
+                    operator = "+";
+                    result = operand1+operand2;
+                    break;
+                case 2:
+                    operator = "-";
+                    result = operand1-operand2;
+                    break;
+                case 3:
+                    operator = "*";
+                    result = operand1*operand2;
+                    break;
+            }
+
+        // Zufallsaufgabe wird so lange neu erstellt, bis Ergebnis größer oder gleich 0 ist
         } while (result < 0);
 
-        System.out.println(operand1 + operator + operand2);
-
+        // printet die generierte Rechenaufgabe
+        System.out.println(operand1 + " " + operator + " " + operand2 + " = ?");
     }
 
+    // Getter-Methode zur Rückgabe des Ergebnisses der zufälligen Rechenaufgabe
     public int getResult() {
         return result;
     }
